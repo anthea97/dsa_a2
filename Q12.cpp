f39da9668cb8259929d4b28306be5d0c58c342a0
Anthea Abreo <anthea.abreo@gmail.com>
2022-11-18 16:19:51 -0600

    Add Q12

--- Q12.cpp
+++ Q12.cpp
@@ -1,5 +1,5 @@
-/* Topic: Reverse a Linked List
- * Question: Contains Duplicate
+/* Topic: Linked List
+ * Question: Reverse a Linked List
  * Given an integer array nums,
  * return true if any value appears at least twice in the array,
  * and return false if every element is distinct.
