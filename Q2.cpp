38c79e32ca73d33e4c4c2b15745d0738d6128ab0
Anthea Abreo <anthea.abreo@gmail.com>
2022-11-18 08:46:14 -0600

    Add Q2. Longest Consecutive Sequence

--- Q2.cpp
+++ Q2.cpp
@@ -1,4 +1,4 @@
-/* Topic: Arrays
+/* Topic: Hashing
  * Question: Longest Consecutive Sequence
  * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
  * You must write an algorithm that runs in O(n) time.
