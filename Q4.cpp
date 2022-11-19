5ed0ab0c17d4698b2b800b14f29c8e64d8ed15e5
Anthea Abreo <anthea.abreo@gmail.com>
2022-11-18 22:08:40 -0600

    Modify Q4

--- Q4.cpp
+++ Q4.cpp
@@ -51,7 +51,36 @@ ListNode* removeNthFromEnd(ListNode* head, int n) {
 
 }
 
+void printLL(ListNode* head){
+    if(head == NULL){
+        cout<<"NULL";
+        return;
+    }
+    ListNode* newnode = head;
+
+    while(newnode!= NULL){
+        cout<<newnode->val<<" ";
+        newnode = newnode->next;
+    }
+}
+
 int main(){
 
-    return 0;
+    ListNode* node1 = new ListNode(1);
+    ListNode* node2 = new ListNode(2);
+    ListNode* node3 = new ListNode(3);
+    ListNode* node4 = new ListNode(4);
+    ListNode* node5 = new ListNode(5);
+    ListNode* head = new ListNode(0);
+
+    node5->next = NULL;
+    node4->next = node5;
+    node3->next = node4;
+    node2->next = node3;
+    node1->next = node2;
+    head->next = node1;
+    printLL(head);
+    cout<<endl;
+    ListNode* newhead = removeNthFromEnd(head, 3);
+    printLL(newhead);
 }
